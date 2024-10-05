33a34f8f0e8a8ff389f38c2b5c48456f3144f804
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-10-05 21:14:35 +0200

    Refactoring configuration

--- src/discipline.core/Configuration/Extensions.cs
+++ src/discipline.core/Configuration/Extensions.cs
@@ -1,11 +1,35 @@
 using discipline.core.Communication.SignalR.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 
 namespace discipline.core.Configuration;
 
 public static class Extensions
 {
+    private const string CorsName = "discipline-notifications-cors";
+    
     public static IServiceCollection AddCore(this IServiceCollection services)
         => services
             .AddBroadcasting();
+
+    private static IServiceCollection AddDisciplineCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("test", policy =>
+            {
+                policy
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithExposedHeaders("X-Pagination");
+            });
+        });
+        return services;
+    }
+
+    // private static WebApplication UseDisciplineCors(this WebApplication app)
+    // {
+    //     
+    // }
 }
