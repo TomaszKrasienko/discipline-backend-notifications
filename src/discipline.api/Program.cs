ab9f55bb3ff9d92f69d62af369be88c42375bf07
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-10-05 19:03:29 +0200

    Added using cors

--- src/discipline.api/Program.cs
+++ src/discipline.api/Program.cs
@@ -7,9 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCore();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("test", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithExposedHeaders("X-Pagination");
+    });
+});
 var app = builder.Build();
-
+app.UseCors("test");
 app.MapPost("/", (IHubContext<NotificationHub> context) =>
 {
     context.Clients.All.SendAsync("test", "test");
